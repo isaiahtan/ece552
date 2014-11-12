6b9e3d77720d0d2cfc7aa89ca0d4e7971dc6dc2e
Isaiah Tan <isaiahtyj@gmail.com>
2014-11-12 18:45:27 -0500

    final updates to mbq1

--- lab4/simplesim-3.0d-assig4/mbq1.c
+++ lab4/simplesim-3.0d-assig4/mbq1.c
@@ -2,19 +2,20 @@
 
 int main(int argc, char *argv[]) {
 
-	// NLP is 64 blks in cache, 64 items per blk
+	// NLP is 64 sets in cache, 2-way, 64 items per blk
 
-	int array_size = 1000000;
+	int next_access = atoi(argv[1]);
+	
+	int array_size = 100000 * next_access;
 	char array [array_size];
 	int i, j;
 
-	int next_access = atoi(argv[1]);
 
 	// have extremely low miss rate (only due to other stack variables) if next_access is within this/next blk size
-	// Eg next_access = 10, miss rate = 0.78%
+	// Eg next_access = 10, miss rate = 0.0%
 
 	// much larger miss rate when skipping over prefetched blk (where next_access >= blk * 2)
-	// Eg next_access = 128, miss rate = 9.87%
+	// Eg next_access = 128, miss rate = 9.96%
 
 	// note results are skewed due to other stack variables, set up, etc.
 
