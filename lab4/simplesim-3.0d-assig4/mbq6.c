af531d72610b0186dede0bbd23015cdee1053371
Isaiah Tan <isaiahtyj@gmail.com>
2014-11-13 01:08:48 -0500

    mbq6

---
+++ lab4/simplesim-3.0d-assig4/mbq6.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+
+	// stride is 64*4 blks in cache, 64B per blk
+	// 128 RPT entries, 
+	
+	// history with max of 2^10 entries
+	
+	int array_size = 10000000;
+	char array [array_size];
+	int i, j;
+
+	
+	for (j = 0; j < 1000; j++){ // multiple loops to resimulate sudo random. will not be captured by stride, but by history.
+		int index = 73; // random prime number
+		
+		for (i = 0; i < 100; i++){ 
+			array[index] ++;
+			index = (index*37 + i*11)%array_size; // next index is sudo random
+		}
+	}
+	return 0;
+}
